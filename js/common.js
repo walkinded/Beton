51fbdb9ec8aedc7187a5345d5de79866d8538acf
Roman <labunskij1997@gmail.com>
2021-06-25 18:24:12 +0300

    done

--- js/common.js
+++ js/common.js
@@ -122,3 +122,18 @@ $('.services__slider').slick({
     }
   ]
 });
+
+$(document).ready(function($) {
+  $('.tab_content').hide();
+  $('.tab_content:first').show();
+  $('.tabs li:first').addClass('active');
+  $('.tabs li').click(function(event) {
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide();
+
+    var selectTab = $(this).find('a').attr("href");
+
+    $(selectTab).fadeIn();
+  });
+});
